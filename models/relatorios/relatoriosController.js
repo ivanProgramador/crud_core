6ea1f99d9c00c7201efaf87a1bad33032459b2a1
ivan <ivan-guita@hotmail.com>
2024-06-18 23:15:54 -0300

    controller de relatorios iniciado

---
+++ models/relatorios/relatoriosController.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const Produto = require("../Produto/Produto");
+const Cargo = require("../cargo/Cargo");
+const Estoque = require("../Estoque/Estoque");
+
+router.get("/relatorio_produto",(req,res)=>{
+    
+
+});
+
+router.get("/relatorio_cargos",(req,res)=>{
+  
+});
+
+router.get("/relatorio_estoque",(req,res)=>{
+
+});
+
+
+
+
+module.exports = router;
