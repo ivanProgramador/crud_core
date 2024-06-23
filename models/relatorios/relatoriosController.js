c77227f72693b3c6e74c2a0b740bc07a778bc3c6
ivan <ivan-guita@hotmail.com>
2024-06-22 22:09:41 -0300

    opções de ordenação prontas /falta  resolver o problema da data

--- models/relatorios/relatoriosController.js
+++ models/relatorios/relatoriosController.js
@@ -3,23 +3,63 @@ const router = express.Router();
 const Produto = require("../Produto/Produto");
 const Cargo = require("../cargo/Cargo");
 const Estoque = require("../Estoque/Estoque");
+const { where } = require("sequelize");
+const { format } = require('date-fns');
 
 router.get("/relatorio_produto",(req,res)=>{
-    Produto.findAll().then(produtos=>{
-        
-        res.render("relatorios/produtos/rel_prod",{produtos:produtos});
-    });
-});
 
-router.get("/relatorio_cargos",(req,res)=>{
-    res.render("/relatorios/cargos/");
-  
-});
+  Produto.findAll().then(produtos=>{
 
-router.get("/relatorio_estoque",(req,res)=>{
-    res.render("/relatorios/estoques/");
+      res.render('relatorios/produtos/rel_prod',{produtos:produtos});
+
+  });
 
 });
+
+router.get("/relatorio_produto/:filtro",(req,res)=>{
+
+  var{filtro} = req.params;
+
+  if(filtro=='cre_valor'){
+
+    Produto.findAll({order:[['preco','ASC']]}).then(produtos=>{
+      res.render('relatorios/produtos/rel_prod',{produtos:produtos});
+  });
+
+  }
+  else if(filtro == 'decre_valor'){
+
+   Produto.findAll({order:[['preco','DESC']]}).then(produtos=>{
+      res.render('relatorios/produtos/rel_prod',{produtos:produtos});
+   });
+  }
+  else if(filtro == 'cre_data'){
+
+    Produto.findAll({order:[['createdAt','ASC']]}).then(produtos=>{
+      res.render('relatorios/produtos/rel_prod',{produtos:produtos});
+   });
+
+  }
+  else if(filtro == 'decre_data'){
+
+    Produto.findAll({order:[['createdAt','DESC']]}).then(produtos=>{
+      res.render('relatorios/produtos/rel_prod',{produtos:produtos});
+   });
+
+
+
+  }
+
+ 
+
+});
+
+
+
+    
+
+
+
 
 
 
